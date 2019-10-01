8a615391840f6b003e053fb771b6be40e5db40c4
Sharina Stubbs <sharinastubbs@gmail.com>
2019-10-01 15:11:13 -0700

    added a couple more tests to album test and added bootstrap link

--- src/test/java/com/sharinas/songr/AlbumTest.java
+++ src/test/java/com/sharinas/songr/AlbumTest.java
@@ -14,6 +14,7 @@ public class AlbumTest {
                 4,
                 90,
                 "https://upload.wikimedia.org/wikipedia/en/a/a3/Birdy_-_Fire_Within.png");
+
         assertEquals("The album Amazingness Music is by Peter Piper. The album has 4 songs and is 90 seconds long.",
                 "The album Amazingness Music is by Peter Piper. The album has 4 songs and is 90 seconds long.",
                 testAlbum.toString());
@@ -31,5 +32,25 @@ public class AlbumTest {
         assertEquals("Should expect the title of Amazingness Music",
                 "Amazingness Music",
                 testAlbum.title);
+        assertEquals("Peter Piper", testAlbum.artist);
+        assertEquals(4, testAlbum.songCount);
+        assertEquals(90, testAlbum.lengthInSec);
+
     }
+
+    public void testSetTitle() {
+        Album testAlbum = new Album (
+            "Amazingness Music",
+                    "Peter Piper",
+                    4,
+                    90,
+                    "https://upload.wikimedia.org/wikipedia/en/a/a3/Birdy_-_Fire_Within.png");
+            testAlbum.setTitle("Terrible Music");
+
+            assertEquals("Terrible Music",
+                    testAlbum.title
+                    );
+
+    }
+
 }
