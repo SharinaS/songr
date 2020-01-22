c19fa28044c8e0e4bb82020e46259defccf657b0
Sharina Stubbs <sharinastubbs@gmail.com>
2020-01-21 17:15:16 -0800

    added fragments and updated head to home and album to use a fragment. Added comments to code

--- src/main/java/com/sharinas/songr/models/AlbumRepository.java
+++ src/main/java/com/sharinas/songr/models/AlbumRepository.java
@@ -2,5 +2,6 @@ package com.sharinas.songr.models;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-//JpaRespository <Type, IDentifier Type
+//JpaRespository <Type, IDentifier Type>
+// This interface is a repository that has the job of interacting with the database.
 public interface AlbumRepository extends JpaRepository<Album, Long> { }
